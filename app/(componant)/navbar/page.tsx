75a28eb358bf42561660f8382cd37afa8cf20f16
devmianharoon <devmianharoon@gmail.com>
2023-12-01 15:26:21 +0500

    format

--- app/(componant)/navbar/page.tsx
+++ app/(componant)/navbar/page.tsx
@@ -7,8 +7,8 @@ export default function Navbar() {
     return (
         <>
             <section className=' px-5 py-6 flex gap-2 justify-between md:px-16 md:pt-14 md:pb-0  bg-white shadow-md'>
-            <Link href={'/'}><Image src={logo} alt='logo' className='xl w-[396px] h-[37.5px]' /></Link>
-                <Image src={menu} alt='logo' className='md:hidden cursor-pointer' />
+                <Link href={'/'}><Image src={logo} alt='logo' className='xl w-[396px] h-[37.5px]' /></Link>
+                <Image src={menu} alt='menu' className='md:hidden cursor-pointer' />
                 <ul className=' sm:hidden hidden md:flex  gap-8'>
                     <Link href={'/blog'}><li className='font-new text-black text-center text-2xl font-normal md:font-medium leading-[170%] tracking-wider uppercase hover:border-b-[6px] hover:border-[#000000] pb-8'>Blog</li></Link>
                     <Link href={'/about'}><li className='font-new text-black text-center text-2xl font-normal md:font-medium leading-[170%] tracking-wider uppercase hover:border-b-[6px] hover:border-[#000000] pb-8'>About</li></Link>
